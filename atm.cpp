84ab01e43d2031ee59a7487438564b175e336883
znm3m8 <znm3m8@mst.edu>
2017-05-02 20:59:27 -0500

    fixed bugs in atm.cpp file

--- atm.cpp
+++ atm.cpp
@@ -6,24 +6,29 @@ using namespace std;
 double atm::temp(double H)
 {
 	double T;
-	int US_H [7] = {0, 11000, 20000, 32000, 47000, 51000, 71000}
+	int US_H [8] = {0, 11000, 20000, 32000, 47000, 51000, 71000, 86000};
 	double US_T [7] = {288.15, 216.65, 216.65, 228.65, 270.65, 270.65, 214.65};
 	double US_R [7] = {-.0065, 0, .001, .0028, 0, -.0028, -.002};
 	
-	if(H < US_H(1))
-		T = US_T(0) + US_R(0)*(H-US_H(0));
-	elseif(H < US_H(2))
-		T = US_T(1) + US_R(1)*(H-US_H(1));
-	elseif(H < US_H(3))
-		T = US_T(2) + US_R(2)*(H-US_H(2));
-	elseif(H < US_H(4))
-		T = US_T(3) + US_R(3)*(H-US_H(3));	
-	elseif(H < US_H(5))
-		T = US_T(4) + US_R(4)*(H-US_H(4));
-	elseif(H < US_H(6))
-		T = US_T(5) + US_R(5)*(H-US_H(5));
+
+	if(H < US_H[ 1 ])
+		T = US_T[ 0 ] + US_R[ 0 ]*(H-US_H[ 0 ]);
+	else if(H < US_H[ 2 ])
+		T = US_T[ 1 ] + US_R[ 1 ]*(H-US_H[ 1 ]);
+	else if(H < US_H[ 3 ])
+		T = US_T[ 2 ] + US_R[ 2 ]*(H-US_H[ 2 ]);
+	else if(H < US_H[ 4 ])
+		T = US_T[ 3 ] + US_R[ 3 ]*(H-US_H[ 3 ]);	
+	else if(H < US_H[ 5 ])
+		T = US_T[ 4 ] + US_R[ 4 ]*(H-US_H[ 4 ]);
+	else if(H < US_H[ 6 ])
+		T = US_T[ 5 ] + US_R[ 5 ]*(H-US_H[ 5 ]);
+	else if(H < US_H[ 7 ])
+		T = US_T[ 6 ] + US_R[ 6 ]*(H-US_H[ 6 ]);
 	else
-		T = US_T(6) + US_R(6)*(H-US_H(6));	
+		T = US_T[ 6 ] + US_R[ 6 ]*(US_H[ 7 ] - US_H[ 6 ]);
+
+
 	return T;
 }
 
